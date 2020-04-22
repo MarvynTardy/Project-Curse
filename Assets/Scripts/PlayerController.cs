927c616dc1edd64f25e0a10d3a2c311c91c2149e
Arthur Craste <acraste@gmail.com>
2020-04-22 23:46:29 +0200

    Controller des mouvements de base fini et début de caméra

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
     public float moveSpeed = 8;
     public float jumpForce = 150;
 
@@ -12,6 +13,13 @@ public class PlayerController : MonoBehaviour
     public CharacterController controller;
     private Vector3 m_MoveDirection;
 
+    /*
+    // Gestion du dash
+    public Vector3 Drag = new Vector3(1, 1, 1);
+    public float DashDistance = 100;
+    public Vector3 _velocity = new Vector3 (10, 10, 10);
+    */
+
     void Start()
     {
         controller = GetComponent<CharacterController>();
@@ -19,8 +27,22 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        // ↓ Permet de stocker la valeur du déplacement en y
+        float yStore = m_MoveDirection.y;
+
+        // ↓ Permet de gérer la direction du personnage relativement à sa position dans l'espace
+        m_MoveDirection = (transform.forward * Input.GetAxisRaw("Vertical")) + (transform.right * Input.GetAxisRaw("Horizontal"));
+
+        // ↓ Permet de gérer le déplacement en diagonal
+        m_MoveDirection = m_MoveDirection.normalized * moveSpeed;
+
+        // ↓ Restaure la valeur de y avant qu'il soit Normalized pour pas que le normalized n'affecte l'axe y
+        m_MoveDirection.y = yStore;
+
+
+
         // On incrémente le vecteur de direction en fonction de l'axe sur laquelle le joueur appuis (paramétrer dans le Settings de Unity)
-        m_MoveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, 0, Input.GetAxis("Vertical") * moveSpeed);
+        // m_MoveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, m_MoveDirection.y, Input.GetAxis("Vertical") * moveSpeed);
 
         if (controller.isGrounded)
         {
@@ -31,9 +53,22 @@ public class PlayerController : MonoBehaviour
         }
 
         // Gestion de la chute en l'air
-        m_MoveDirection.y = m_MoveDirection.y + (Physics.gravity.y * gravityScale);
+        m_MoveDirection.y = m_MoveDirection.y + (Physics.gravity.y * gravityScale * Time.deltaTime);
 
         // On applique le vector de direction à la fonction préfaite du CC move qui gère sa direction et vélocité
         controller.Move(m_MoveDirection * Time.deltaTime);
+
+        /*if (Input.GetButtonDown("Dash"))
+        {
+            Debug.Log("Dash");
+            _velocity += Vector3.Scale(transform.forward,
+                                       DashDistance * new Vector3((Mathf.Log(1f / (Time.deltaTime * Drag.x + 1)) / -Time.deltaTime),
+                                                                  0,
+                                                                  (Mathf.Log(1f / (Time.deltaTime * Drag.z + 1)) / -Time.deltaTime)));
+        }
+
+        _velocity.x /= 1 + Drag.x * Time.deltaTime;
+        _velocity.y /= 1 + Drag.y * Time.deltaTime;
+        _velocity.z /= 1 + Drag.z * Time.deltaTime;*/
     }
 }
---
+++ Assets/Scripts/Unuse Scripts/CameraControler.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControler : MonoBehaviour
+{
+    public Transform target;
+
+    // ↓ variable utilisé pour créer le décalage entre la position du joueur et celle de la caméra
+    public Vector3 cameraOffset;
+
+    public bool useOffsetValues;
+
+    public float rotateSpeed;
+
+    public Transform pivot;
+
+    void Start()
+    {
+        if (!useOffsetValues)
+        {
+            cameraOffset = target.position - transform.position;
+        }
+
+        /*
+        pivot.transform.position = target.transform.position;
+        pivot.transform.parent = target.transform;*/
+
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+
+    void LateUpdate()
+    {
+        // Récupère la position de la souris & rotate la target
+        float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
+        target.Rotate(0, horizontal, 0);
+
+        /*
+        // Récupère la position de la souris & rotate la target
+        float vertical = Input.GetAxis("Mouse Y") * rotateSpeed;
+        pivot.Rotate(-vertical, 0, 0);*/
+
+        float desiredYAngle = target.eulerAngles.y;
+        /*float desiredXAngle = pivot.eulerAngles.x;*/
+
+        Quaternion rotation = Quaternion.Euler(0, desiredYAngle, 0);
+        transform.position = target.position - (rotation * cameraOffset);
+
+        transform.LookAt(target);
+    }
+}
