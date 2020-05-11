72d796dc3e03e8ec30350451bc1f1218c88bab53
Yoan Bellec--Ancona <Oriigine@hotmail.com>
2020-05-11 16:14:56 +0100

    Health Bar (Droite et pas en demi-cercle) ennemi faite.

---
+++ Assets/Scripts/billboard.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class billboard : MonoBehaviour
+{
+    public Transform Cam;
+
+    private void LateUpdate()
+    {
+        transform.LookAt(transform.position + Cam.forward);
+    }
+}
