0e0ca4141c314ec2aae785e27791300e67476d51
MarvynTardy <marvyn.tardy@e-artsup.net>
2020-05-12 15:01:55 +0200

    début 2eme enemy  + 1er enemy target générique

--- Assets/Scripts/Enemy/EnemyDasher.cs
+++ Assets/Scripts/Enemy/EnemyDasher.cs
@@ -4,44 +4,65 @@ using UnityEngine;
 
 public class EnemyDasher : MonoBehaviour
 {
+
     public Animator animMonster;
     // Variables Dash
     public float dashForce;
     public float dashTime;
     public float currentTimeBeforeDash = 0;
     public float timeBeforeDash = 2;
+    public float detectionRange = 10;
     private Rigidbody rb;
     public Transform player;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-       
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(currentTimeBeforeDash < timeBeforeDash)
+        float distance = Vector3.Distance(player.transform.position, transform.position);
+        if (distance <= detectionRange)
         {
-            currentTimeBeforeDash += Time.deltaTime;
+            if (currentTimeBeforeDash < timeBeforeDash)
+            {
+                animMonster.SetBool("isCharge",true);
+                currentTimeBeforeDash += Time.deltaTime;
+            }
+            else
+            {
+                StartCoroutine(Dash());
+                currentTimeBeforeDash = 0;
+                
+            }
+            transform.LookAt(player.transform.position);
         }
         else
         {
-            StartCoroutine(Dash());
+            animMonster.SetBool("isCharge", false);
             currentTimeBeforeDash = 0;
         }
-        transform.LookAt(player.transform.position);
+
     }
-    
-    
-        
-    
+
+
+
+
     public IEnumerator Dash()
     {
+        animMonster.SetTrigger("isAttack");
         rb.AddForce(transform.forward * dashForce, ForceMode.VelocityChange);
         yield return new WaitForSeconds(dashTime);
         rb.velocity = Vector3.zero;
-        animMonster.SetTrigger("isAttack");
+
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, detectionRange);
+
     }
 }
