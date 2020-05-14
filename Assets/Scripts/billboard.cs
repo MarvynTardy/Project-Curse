254315e67cf51b0d88f97a2510a1f79c34d6f07e
Yoan Bellec--Ancona <Oriigine@hotmail.com>
2020-05-14 16:50:14 +0100

    [HUD]Hud de l'énnemi retravaillé, il est placé correctement :) PS: c'est jeremy qui en me voyant galéré m'a mis sur la voie de la solution, alors que même achille a laissé tombé.... jeremy carry se projet même sur le code jpp.

--- Assets/Scripts/billboard.cs
+++ Assets/Scripts/billboard.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class billboard : MonoBehaviour
 {
-    public Transform Cam;
+    private Camera Cam;
 
+    private void Start()
+    {
+        Cam = FindObjectOfType<Camera>();
+    }
     private void LateUpdate()
     {
-        transform.LookAt(transform.position + Cam.right);
+        transform.LookAt(new Vector3(Cam.transform.position.x, -90, Cam.transform.position.z));
+
     }
 }
